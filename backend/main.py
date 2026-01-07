adaab59c473ca68975c5fae683483fafd84d3162
beepobb <yeejiazhen23@gmail.com>
2026-01-07 22:55:11 +0800

    use env variables

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+import os 
+FRONTEND_URL = os.getenv("VITE_FRONTEND_URL") 
 
 app = FastAPI()
 
+# Allow React dev server
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"message": "It works"}
+    return {"message": "Hello"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Hello, World!");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.message);
+        setMessage(data.message);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "2rem" }}>
+      <h1>My Prototype Frontend</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
